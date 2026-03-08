7462ccd08f2aceefc0d9c2d7ac28569589f348ca
Ahmet <103840089+temha888@users.noreply.github.com>
2026-03-08 21:02:41 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T18:02:35.748Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-08T18:02:39.422Z
+Commit: Fix CSS styling bug on mobile devices

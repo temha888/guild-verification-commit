fece72e3c449a484a6ebe58f0d8450bf9730eaea
Ahmet <103840089+temha888@users.noreply.github.com>
2026-03-08 21:05:56 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T18:02:33.535Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-08T18:05:54.392Z
+Commit: Patch API endpoint error handling

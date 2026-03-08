2779e528b3a7d0da57e6c7e314a50ceecc3e962a
Ahmet <103840089+temha888@users.noreply.github.com>
2026-03-08 21:07:20 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T18:06:58.771Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-08T18:07:18.362Z
+Commit: Resolve null pointer exception in user service

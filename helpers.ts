a01a104e3dedcd63ed0e56b1d4c74b4bb22ef043
Ahmet <103840089+temha888@users.noreply.github.com>
2026-03-08 21:07:01 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T18:05:54.392Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-08T18:06:58.771Z
+Commit: Fix memory leak in event handlers

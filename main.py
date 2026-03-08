443b32ac0490137209f719999e364a15a31b8d53
Ahmet <103840089+temha888@users.noreply.github.com>
2026-03-08 21:06:54 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T18:06:36.914Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-08T18:06:51.993Z
+Commit: Fix broken links in documentation

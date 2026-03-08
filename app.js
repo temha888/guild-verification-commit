12a7c8a2e2244df3c87470eafd4d72411cd7ae63
Ahmet <103840089+temha888@users.noreply.github.com>
2026-03-08 21:05:59 +0300

    Resolve timezone issues in date handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T18:03:29.225Z
-Commit: Add multi-language support
+Timestamp: 2026-03-08T18:05:57.683Z
+Commit: Resolve timezone issues in date handling

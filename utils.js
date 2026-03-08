5eac50ea0f76b2d55f458e91e4ba61f26ecf7b14
Ahmet <103840089+temha888@users.noreply.github.com>
2026-03-08 21:01:48 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T18:01:07.097Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-08T18:01:46.005Z
+Commit: Fix CSS styling bug on mobile devices

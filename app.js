da3d3d43461ea95263401cc1c3507684b6922612
Ahmet <103840089+temha888@users.noreply.github.com>
2026-03-08 21:03:13 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T18:03:05.183Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-08T18:03:11.358Z
+Commit: Add multi-language support

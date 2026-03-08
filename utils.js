00a8776cea95e105d8a57f706fdc14e2dd018351
Ahmet <103840089+temha888@users.noreply.github.com>
2026-03-08 21:02:12 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T18:02:07.131Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-08T18:02:10.553Z
+Commit: Fix broken links in documentation

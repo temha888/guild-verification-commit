3c3b450b873ace9fe7bfc7141bb17923a2ea5390
Ahmet <103840089+temha888@users.noreply.github.com>
2026-03-08 21:07:25 +0300

    Fix database connection timeout

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T18:07:20.686Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-08T18:07:22.909Z
+Commit: Fix database connection timeout

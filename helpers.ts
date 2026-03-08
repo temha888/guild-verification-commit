fc475a105039f027da27894e60140f31476cd12f
Ahmet <103840089+temha888@users.noreply.github.com>
2026-03-08 21:01:37 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T18:01:24.424Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-08T18:01:35.382Z
+Commit: Patch API endpoint error handling

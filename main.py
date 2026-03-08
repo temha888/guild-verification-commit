f98bc7c8ff074c5f5529959f42a42df5b384c8d3
Ahmet <103840089+temha888@users.noreply.github.com>
2026-03-08 21:06:39 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T18:06:25.977Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-08T18:06:36.914Z
+Commit: Fix CSS styling bug on mobile devices
